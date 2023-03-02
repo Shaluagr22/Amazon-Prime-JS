65241b374eac3d429d0a639d4e28cf7e3e1a1551
Shalini <shalu.garg22@gmail.com>
2023-03-02 20:39:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const favChannels = [
+    {img_url : 'img/channels/Discovery.jpg'},
+    {img_url : 'img/channels/Docubay.jpg'},
+    {img_url : 'img/channels/Erosnow.jpg'},
+    {img_url : 'img/channels/Hayu.jpg'},
+    {img_url : 'img/channels/hoichoi.jpg'},
+    {img_url : 'img/channels/LionsgatePlay.jpg'},
+    {img_url : 'img/channels/MANORAMAMAX.jpg'},
+    {img_url : 'img/channels/Mubi.jpg'},
+    {img_url : './img/channels/ShortsTV.jpg'},
+]
+
+favChannels.map((item)=>{
+    var div = document.createElement('div')
+    div.setAttribute("class","channelDiv")
+
+    var image = document.createElement("img")
+    img.setAttribute("src",item.img_url)
+
+    div.append(image);
+    document.querySelector(".channelBtn").append(div)
+})
+
+localStorage.setItem("favChannels",JSON.stringify(favChannels));
